b59a75053cab493acfc0b57b3f19a3d01adbd650
Jyotishman saikia <jyotishmansaikia@Jyotishmans-MacBook-Pro.local>
2019-11-17 23:38:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TableApp from "./TableApp";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.options = {
+      colDefs : [{
+        "label": "Text Columns",
+        "width": "40 % ",
+        "type": "text"
+      },
+      {
+        "label": "Input Column",
+        "width": "60 % ",
+        "type": "input"
+      },
+      {
+        "label": "Checkbox Column",
+        "width": "30 px",
+        "type": "checkbox"
+      }],
+      data: [{
+        "id": "1a",
+        "colData": ["Laxman", "Bangalore, India", true]
+      },
+      {
+        "id": "2a",
+        "colData": ["Ram", "Delhi, Malviya Nagar", false]
+      }, {
+        "id": "3a",
+        "colData": ["Jyotishman", "Noida, Sector 27", true]
+      }, {
+        "id": "4a",
+        "colData": ["Pankaj", "Gurgaon, India", false]
+      }, {
+        "id": "5a",
+        "colData": ["Ashish", "UP, India", true]
+      }, {
+        "id": "6a",
+        "colData": ["Pulkit", "Pune, India", true]
+      }]
+    }
+  }
+  update () {
+    var index = prompt("Which row  you want to update", "");
+    if(index)
+      this.refs.child.update(index-1);
+  }
+  delete() {
+    var index = prompt("Which row  you want to delete", "");
+    if(index)
+      this.refs.child.delete(index-1);
+  }
+  add() {
+    this.refs.child.add(this.options.data);
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "1px solid #000", display: "block", margin: "auto", maxWidth: "500px"}}>
+        <h3>Inside Table Component</h3>
+        <TableApp options = {this.options}  ref="child"/>
+      </div>
+
+      <div className="btn-wrap">
+        <p>Main App</p>
+        <button onClick={()=> this.add()}>Add more</button>
+        <button onClick={()=> this.delete()}>Delete</button>
+        <button onClick={()=> this.update()}>Update</button>
+      </div>
+      
     </div>
   );
+  }
 }
 
 export default App;
