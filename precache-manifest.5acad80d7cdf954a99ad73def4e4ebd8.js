9e4a41fd4aa8ef854ac15e2e635a8156c4a77934
Jyotishman saikia <jyotishmansaikia@Jyotishmans-MacBook-Pro.local>
2019-11-17 23:47:38 +0530

    build pages

---
+++ precache-manifest.5acad80d7cdf954a99ad73def4e4ebd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad382db6905f5cc51ce933b8986a57bc",
+    "url": "/ver-table/index.html"
+  },
+  {
+    "revision": "b65a84bdb1b67d3dcec0",
+    "url": "/ver-table/static/css/main.6beb73d0.chunk.css"
+  },
+  {
+    "revision": "fddae81a1d0f442a4b4a",
+    "url": "/ver-table/static/js/2.f3432f45.chunk.js"
+  },
+  {
+    "revision": "b65a84bdb1b67d3dcec0",
+    "url": "/ver-table/static/js/main.3bd3f25c.chunk.js"
+  },
+  {
+    "revision": "90240069cd73a2917733",
+    "url": "/ver-table/static/js/runtime-main.b5aa6bbe.js"
+  }
+]);
